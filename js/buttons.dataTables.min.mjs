5cdef8d56a1c62d8b2e0fb5662812d74f72d13a2
Allan Jardine <allan.jardine@sprymedia.co.uk>
2023-05-29 15:26:06 +0100

    582751d57e927f08a894058518561bf0f9adba97 New: Dark mode support for DataTables styling
    Sync to source repo @582751d57e927f08a894058518561bf0f9adba97

--- js/buttons.dataTables.min.mjs
+++ js/buttons.dataTables.min.mjs
@@ -1,4 +1,4 @@
 /*! DataTables styling wrapper for Buttons
  * ©2018 SpryMedia Ltd - datatables.net/license
  */
-import $ from"jquery";import DataTable from"datatables.net-dt";import Buttons from"datatables.net-buttons";export default DataTable;
+import jQuery from"jquery";import DataTable from"datatables.net-dt";import Buttons from"datatables.net-buttons";let $=jQuery;export default DataTable;
--- js/buttons.dataTables.mjs
+++ js/buttons.dataTables.mjs
@@ -2,9 +2,12 @@
  * ©2018 SpryMedia Ltd - datatables.net/license
  */
 
-import $ from 'jquery';
+import jQuery from 'jquery';
 import DataTable from 'datatables.net-dt';
 import Buttons from 'datatables.net-buttons';
+
+// Allow reassignment of the $ variable
+let $ = jQuery;
 
 
 
