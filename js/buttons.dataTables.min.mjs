1afec6b1a631cbb193672c6ce19bfbc1b782b947
dtbuild <dtbuild@rigil>
2026-09-25 11:00:57 +0000

    0019262b2fbd4fb29bfa3746b8618ac680068db8 Release 4.1.2
    Sync to source repo @0019262b2fbd4fb29bfa3746b8618ac680068db8

--- js/buttons.dataTables.min.mjs
+++ js/buttons.dataTables.min.mjs
@@ -1,4 +1,4 @@
-/*! Buttons default styling 4.1.1 for DataTables
+/*! Buttons default styling 4.1.2 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 import DataTable from"datatables.net-dt";import Buttons from"datatables.net-buttons";export default DataTable;
